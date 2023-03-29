326ea7726de13747c9c91f59b44117999046a536
Samuel Tessema <stessema@LABA.PSWAS.LOCAL>
2023-03-29 14:06:45 -0700

    Test Document

--- test.js
+++ test.js
@@ -5,6 +5,12 @@ console.log('This is update 1')
 
 console.log('This is update 2')
 
+console.log('This is update 5')
 
+let names = ['John', 'Jane', 'Mary']
+
+names.forEach( (name) => {
+    console.log("hey", name)
+})
 
 
